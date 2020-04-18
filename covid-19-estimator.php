bd626d1f9d004f1b19b0f00936c577c93be087d9
AbdulHakeem AbdulAleem <hakeemtunde@hotmail.com>
2020-04-18 03:40:21 +0100

    fix typo

--- covid-19-estimator.php
+++ covid-19-estimator.php
@@ -8,13 +8,13 @@ function covid19ImpactEstimator($data)
   $avg_daily_income_in_usd = $data->region->avgDailyIncomeInUSD;
 
   $impact['impact']['currentlyInfected'] = $reported_cases * 10;
-  $sever_impact['severImpact']['currentlyInfected'] = $reported_cases * 50;
+  $severe_impact['severImpact']['currentlyInfected'] = $reported_cases * 50;
 
   //projection in 30day (after 3days 2^10)
   $impact['impact']['infectionsByRequestedTime'] = $impact['impact']['currentlyInfected'] * 1024;
-  $sever_impact['severImpact']['infectionsByRequestedTime'] = $sever_impact['severImpact']['currentlyInfected'] * 1024;
+  $severe_impact['severImpact']['infectionsByRequestedTime'] = $severe_impact['severImpact']['currentlyInfected'] * 1024;
 
-  $sever_cases = $impact['impact']['infectionsByRequestedTime'] * 0.15;
+  $severe_cases = $impact['impact']['infectionsByRequestedTime'] * 0.15;
 
   //casesForICUByRequestedTime
   $icu_care_cases = $impact['impact']['infectionsByRequestedTime'] * 0.05;
@@ -26,14 +26,14 @@ function covid19ImpactEstimator($data)
   $dollars_in_flight = ($impact['impact']['infectionsByRequestedTime'] * $avg_daily_income_population) * $avg_daily_income_in_usd * 30;
 
   $available_hospital_beds = $total_hospital_beds * 0.35;
-  $hospital_beds_by_requested_time = $available_hospital_beds - $sever_cases;
+  $hospital_beds_by_requested_time = $available_hospital_beds - $severe_cases;
 
   $dd['data']['impact']['currentlyInfected']= $impact['impact']['currentlyInfected'];
   $dd['data']['impact']['infectionsByRequestedTime'] = $impact['impact']['infectionsByRequestedTime'];
-  $dd['data']['severImpact']['currentlyInfected'] = $sever_impact['severImpact']['currentlyInfected'];
-  $dd['data']['severImpact']['infectionsByRequestedTime'] = $sever_impact['severImpact']['infectionsByRequestedTime'];
+  $dd['data']['severImpact']['currentlyInfected'] = $severe_impact['severImpact']['currentlyInfected'];
+  $dd['data']['severImpact']['infectionsByRequestedTime'] = $severe_impact['severImpact']['infectionsByRequestedTime'];
   $dd['data']['hospitalBedsByRequestedTime'] = $hospital_beds_by_requested_time;
-  $dd['data']['severeCasesByRequestedTime'] = $sever_cases;
+  $dd['data']['severeCasesByRequestedTime'] = $severe_cases;
   $dd['data']['casesForICUByRequestedTime'] = $icu_care_cases;
   $dd['data']['casesForVentilatorsByRequestedTime'] = $ven_cases;
   $dd['data']['dollarsInFlight'] = $dollars_in_flight;
